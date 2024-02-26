a415ba971507a769c2eb3d3d9a4052c7f4e5ce86
Richard <code.monkey.ricky@gmail.com>
2024-02-25 20:31:13 -0800

    Working priority example from chatgpt.

--- realtime/main.c
+++ realtime/main.c
@@ -1,57 +1,91 @@
-
-#include <pthread.h>
-#include <sched.h>
 #include <stdio.h>
-#include <stdbool.h>
-#include <assert.h>
+#include <stdlib.h>
+#include <pthread.h>
+#include <unistd.h>
 
-void *high_prio_func(void *arg)
+#define NUM_THREADS 5
+#define THREAD_1_PRIORITY 50
+#define THREAD_2_PRIORITY 60
+
+void *thread1_function(void *arg)
 {
-    pthread_attr_t tattr;
-    pthread_t tid;
-    int rv;
-    struct sched_param param;
+    int thread_number = *(int *)arg;
+    struct sched_param params;
 
-    /* initialized with default attributes */
-    rv = pthread_attr_init(&tattr);
-    assert(rv == 0);
+    // Set the thread priority
+    params.sched_priority = THREAD_1_PRIORITY;
 
-    rv = pthread_attr_setschedpolicy(&tattr, SCHED_FIFO);
-    assert(rv == 0);
-
-    /* safe to get existing scheduling param */
-    rv = pthread_attr_getschedparam(&tattr, &param);
-    assert(rv == 0);
-
-    rv = pthread_attr_setinheritsched(&tattr, PTHREAD_EXPLICIT_SCHED);
-    assert(rv == 0);
-
-    /* set the priority; others are unchanged */
-    /* setting the new scheduling param */
-    param.sched_priority = 80;
-    rv = pthread_attr_setschedparam(&tattr, &param);
-    assert(rv == 0);
-
-    while (true)
+    if (pthread_setschedparam(pthread_self(), SCHED_FIFO, &params) != 0)
     {
-        // printf("blah \n");
-        // usleep(1000*1000);
+        perror("pthread_setschedparam");
+        exit(EXIT_FAILURE);
     }
+
+    // Infinite loop to simulate a thread doing some work
+    while (1)
+    {
+        // printf("Thread %d is running\n", thread_number);
+        // sleep(1);
+    }
+
+    pthread_exit(NULL);
 }
 
-int main(void)
+void *thread_other_function(void *arg)
 {
-    int rv;
-    pthread_attr_t tattr;
-    pthread_t tid;
+    int thread_number = *(int *)arg;
+    struct sched_param params;
 
-    rv = pthread_attr_init(&tattr);
-    assert(rv == 0);
+    // Set the thread priority
+    params.sched_priority = THREAD_2_PRIORITY;
 
-    /* with new priority specified */
-    rv = pthread_create(&tid, &tattr, high_prio_func, 0);
-    assert(rv == 0);
+    if (pthread_setschedparam(pthread_self(), SCHED_FIFO, &params) != 0)
+    {
+        perror("pthread_setschedparam");
+        exit(EXIT_FAILURE);
+    }
 
-    while (true)
-        usleep(100);
+    // Infinite loop to simulate a thread doing some work
+    while (1)
+    {
+        // printf("Thread %d is running\n", thread_number);
+        // sleep(1);
+    }
+
+    pthread_exit(NULL);
+}
+
+int main()
+{
+    pthread_t threads[NUM_THREADS];
+    int thread_args[NUM_THREADS];
+    int result_code, i;
+
+    // Create two threads
+    for (i = 0; i < NUM_THREADS; i++)
+    {
+        thread_args[i] = i;
+        if (i == 0)
+            result_code = pthread_create(&threads[i], NULL, thread1_function, &thread_args[i]);
+        else 
+            result_code = pthread_create(&threads[i], NULL, thread_other_function, &thread_args[i]);
+        if (result_code != 0)
+        {
+            perror("pthread_create");
+            exit(EXIT_FAILURE);
+        }
+    }
+
+    // Join threads
+    for (i = 0; i < NUM_THREADS; i++)
+    {
+        result_code = pthread_join(threads[i], NULL);
+        if (result_code != 0)
+        {
+            perror("pthread_join");
+            exit(EXIT_FAILURE);
+        }
+    }
+
+    return 0;
 }
